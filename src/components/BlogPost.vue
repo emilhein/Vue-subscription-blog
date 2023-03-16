3ddb8d621c00d23495617628f878f7bdb235e631
emilhein <emilhein1@gmail.com>
2023-03-16 21:45:12 +0100

    1 file modified

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -14,10 +14,28 @@ export default {
   },
   async mounted() {
     let article = await fetch(
-      'https://subscription-server.herokuapp.com/post/' + this.$route.params.id
+      'https://subscription-server.herokuapp.com/post/' + this.$route.params.id,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          customerId: this.getCookie('customerId'),
+        },
+      }
     );
     let serialized = await article.json();
     this.article = serialized;
+  },
+  methods: {
+    getCookie(name) {
+      var nameEQ = name + '=';
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    },
   },
 };
 </script>
